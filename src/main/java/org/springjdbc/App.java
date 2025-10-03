0f873c32ae6d224ad94f27056f026ef286513e89
Sanket Walave <sanketwalave66@gmail.com>
2025-10-03 13:38:01 +0530

    Initial commit - Hibernate Practice Project

--- src/main/java/org/springjdbc/App.java
+++ src/main/java/org/springjdbc/App.java
@@ -11,3 +11,14 @@ public class App
         System.out.println( "Hello World!" );
     }
 }
+
+
+/*
+*
+* git init
+git add .
+git commit -m "Initial commit - Hibernate Practice Project"
+git branch -M main
+git remote add origin https://github.com/SanketWalave/HibernatePractice.git
+git push -u origin main
+*/
